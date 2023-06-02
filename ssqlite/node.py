698c9531122d58b7edf055e3aaa868c62fc64955
BluberryPie <blub3rrypie@gmail.com>
2023-06-02 10:40:30 +0900

    add: getParent function

--- ssqlite/node.py
+++ ssqlite/node.py
@@ -1,4 +1,23 @@
 from abc import ABCMeta, abstractmethod
+from enum import Enum
+
+
+class NodeType(Enum):
+
+    CREATE = 1
+    INSERT = 2
+    UPDATE = 3
+    DROP = 4
+    DELETE = 5
+
+
+class OrphanError(Exception):
+
+    def __init__(self, node_id):
+        self.msg = f"SSqliteNode<{node_id}> has no parent"
+    
+    def __str__(self):
+        return self.msg
 
 
 class SSqliteNode(metaclass=ABCMeta):
@@ -26,9 +45,13 @@ class SSqliteNode(metaclass=ABCMeta):
 
     def getParent(self):
         """Get parent node"""
-        pass
+        parent_node = self.parent
+        if parent_node is None:
+            raise OrphanError(self.node_id)
+        return parent_node
     
-    def getChild(self):
+    @abstractmethod
+    def getChild(self, node_type: NodeType):
         """Get specific child"""
         pass
 
@@ -45,54 +68,82 @@ class SSqliteNode(metaclass=ABCMeta):
 
 class CreateNode(SSqliteNode):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
+
+    def getChild(self, node_type):
+        """Get specific child"""
         pass
 
     def setParent(self):
         """Set parent node"""
+        pass
+
+    def addChild(self):
+        """Add child node"""
         pass
 
 
 class InsertNode(SSqliteNode):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
+
+    def getChild(self, node_type):
+        """Get specific child"""
         pass
 
     def setParent(self):
         """Set parent node"""
+        pass
+
+    def addChild(self):
+        """Add child node"""
         pass
 
 
 class UpdateNode(SSqliteNode):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         pass
 
     def setParent(self):
         """Set parent node"""
+        pass
+
+    def addChild(self):
+        """Add child node"""
         pass
 
 
 class DropNode(SSqliteNode):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         pass
 
     def setParent(self):
         """Set parent node"""
+        pass
+
+    def addChild(self):
+        """Add child node"""
         pass
 
 
 class DeleteNode(SSqliteNode):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         pass
 
     def setParent(self):
         """Set parent node"""
+        pass
+
+    def addChild(self):
+        """Add child node"""
         pass
